8b014a7cf22fa6072fd854e4b67f2fec2759d940
Jakub <lemiszewski@gmx.com>
2020-07-03 12:55:30 +0200

    Added MongoDb connection

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
+// Express
 import express, { Application } from 'express';
+import bodyParser from 'body-parser';
 
 // Importing our controller
 import { Controller } from './main.controller';
-import bodyParser from 'body-parser';
+
+// DB
+import { MONGO_DB } from './constants/WeTaxiApi.constants';
+import mongoose from 'mongoose';
+
+// CORS
 import cors from 'cors';
+
+// Errors
 import { errorHandler } from './middleware/error.middleware';
 import { notFoundHandler } from './middleware/notFound.middleware';
+
+// Logging
 import morganBody from 'morgan-body';
 
 class App {
@@ -18,6 +29,9 @@ class App {
     this.app = express();
     // Main App configuration
     this.setConfig();
+
+    // Mongo configuration
+    this.setMongoConfig();
 
     // Creating and assigning a new instance of our controller
     this.weTaxiController = new Controller(this.app);
@@ -41,6 +55,14 @@ class App {
     // Error Handling
     this.app.use(errorHandler);
     this.app.use(notFoundHandler);
+  }
+
+  // Mongo connection to our DB
+  private setMongoConfig() {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(MONGO_DB, {
+      useNewUrlParser: true,
+    });
   }
 }
 
