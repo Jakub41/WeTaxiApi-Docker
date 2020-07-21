77f6dfbf16a436d9b73f2394cc7e45ce416a2a40
Jakub <lemiszewski@gmx.com>
2020-07-21 14:30:32 +0200

    [WIP] Adding a different Docker approach

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,15 @@ COPY ./.env /usr/src/app/.env
 RUN npm cache clean --force
 RUN npm install
 RUN npm install -g pm2
+ADD . /usr/src/app
 RUN npm run build
+EXPOSE 3000
+ENV NODE_ENV=docker
+CMD [ "npm", "run", "dev", "pm2-runtime", "./dist/server.js"]
 
-ENV NODE_ENV production
-# # ------------------------------------------------------
-# # Production Build
-# # ------------------------------------------------------
+# ------------------------------------------------------
+# Production Build
+# ------------------------------------------------------
 # FROM nginx:1.19.0-alpine
 # RUN apk add --no-cache --repository http://nl.alpinelinux.org/alpine/edge/main libuv \
 #     &&  apk add --update nodejs npm
@@ -23,9 +26,16 @@ ENV NODE_ENV production
 # RUN rm /etc/nginx/conf.d/default.conf
 # RUN rm -rf /docker-entrypoint.d
 # COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
-EXPOSE 3000
+
 # EXPOSE 80
 # #CMD ["nginx", "-g", "daemon off;","npm", "start"]
 # CMD nginx ; exec npm start
 
-CMD ["pm2-runtime", "./dist/server.js"]
+FROM base-builder AS prod
+WORKDIR /usr/src/app
+RUN npm install --production
+COPY --from=base-builder /usr/src/app/dist ./dist
+COPY package* ./
+EXPOSE 8080
+ENV NODE_ENV=production
+CMD ["npm", "start", "pm2-runtime", "./dist/server.js"]
--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,9 @@ import { Controller } from './main.controller';
 
 // DB
 import {
-	MONGO_DB,
-	NUMBER_OF_PARKING_LOTS,
-	NUMBER_OF_AVAILABLE_SLOTS,
+  MONGO_DB,
+  NUMBER_OF_PARKING_LOTS,
+  NUMBER_OF_AVAILABLE_SLOTS,
 } from './constants/WeTaxiApi.constants';
 import mongoose from 'mongoose';
 // models
@@ -26,78 +26,78 @@ import { notFoundHandler } from './middleware/notFound.middleware';
 import morganBody from 'morgan-body';
 
 class App {
-	public app: Application;
+  public app: Application;
 
-	// Declaring our controller
-	public weTaxiController: Controller;
+  // Declaring our controller
+  public weTaxiController: Controller;
 
-	constructor() {
-		this.app = express();
-		// Main App configuration
-		this.setConfig();
+  constructor() {
+    this.app = express();
+    // Main App configuration
+    this.setConfig();
 
-		// Mongo configuration
-		this.setMongoConfig();
+    // Mongo configuration
+    this.setMongoConfig();
 
-		// Creating and assigning a new instance of our controller
-		this.weTaxiController = new Controller(this.app);
+    // Creating and assigning a new instance of our controller
+    this.weTaxiController = new Controller(this.app);
 
-		// Errors always in the bottom as the cycle of Req/Resp closing with it
-		this.setErrors();
-	}
+    // Errors always in the bottom as the cycle of Req/Resp closing with it
+    this.setErrors();
+  }
 
-	private setConfig() {
-		// Allows us to receive requests with data in json format
-		this.app.use(bodyParser.json({ limit: '50mb' }));
-		// Allows us to receive requests with data in x-www-form-urlencoded format
-		this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-		// Enables cors
-		this.app.use(cors());
-		// Morgan Logging
-		morganBody(this.app, { theme: 'darkened' });
-		// initialize parking lot if not present
-		this.initializeParkingLot();
-	}
+  private setConfig() {
+    // Allows us to receive requests with data in json format
+    this.app.use(bodyParser.json({ limit: '50mb' }));
+    // Allows us to receive requests with data in x-www-form-urlencoded format
+    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    // Enables cors
+    this.app.use(cors());
+    // Morgan Logging
+    morganBody(this.app, { theme: 'darkened' });
+    // initialize parking lot if not present
+    this.initializeParkingLot();
+  }
 
-	// Created the parking lots if not present in the initial run
-	private async initializeParkingLot() {
-		await models.ParkingLot.find({})
-			.then((parkingLot) => {
-				if (parkingLot.length === 0) {
-					// create a new parking lots
-					for (let i = 0; i < NUMBER_OF_PARKING_LOTS; i++) {
-						const taxiQueue = [];
-						const parkingLot = new models.ParkingLot({
-							parkingLotName: 'PL:' + i,
-							availableSlots: NUMBER_OF_AVAILABLE_SLOTS,
-							taxiQueue: taxiQueue,
-						});
-						parkingLot.save();
-					}
-				}
-			})
-			.catch((error) => {
-				console.error('Retrieve parking lots failed::', error);
-			});
-	}
+  // Created the parking lots if not present in the initial run
+  private async initializeParkingLot() {
+    await models.ParkingLot.find({})
+      .then((parkingLot) => {
+        if (parkingLot.length === 0) {
+          // create a new parking lots
+          for (let i = 0; i < NUMBER_OF_PARKING_LOTS; i++) {
+            const taxiQueue = [];
+            const parkingLot = new models.ParkingLot({
+              parkingLotName: 'PL:' + i,
+              availableSlots: NUMBER_OF_AVAILABLE_SLOTS,
+              taxiQueue: taxiQueue,
+            });
+            parkingLot.save();
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Retrieve parking lots failed::', error);
+      });
+  }
 
-	private setErrors() {
-		// Error Handling
-		this.app.use(errorHandler);
-		this.app.use(notFoundHandler);
-	}
+  private setErrors() {
+    // Error Handling
+    this.app.use(errorHandler);
+    this.app.use(notFoundHandler);
+  }
 
-	// Mongo connection to our DB
-	private async setMongoConfig() {
-		try {
-			mongoose.Promise = global.Promise;
-			await mongoose.connect(MONGO_DB, {
-				useNewUrlParser: true,
-			});
-		} catch (e) {
-			console.log('Mongo Error >> ', e.message);
-		}
-	}
+  // Mongo connection to our DB
+  private setMongoConfig() {
+    try {
+      mongoose.Promise = global.Promise;
+      mongoose.connect(MONGO_DB, {
+        useNewUrlParser: true,
+      });
+    } catch (e) {
+      console.log('Mongo Error >> ', e.message);
+    }
+  }
 }
 
 export default new App().app;
